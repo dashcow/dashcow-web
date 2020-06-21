dbd200af841955497d5db4b113a018dc0ac1af73
Henry Chang <henry35208@hotmail.com.tw>
2020-06-21 17:15:39 +0800

    test(env): replace jest-sixteen env with mutationobserver-shim due to node 10 support

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+// TODO: remove after create-react-app update jest to v26
+// Add this due to dom-testing-library breaking change, see
+// https://github.com/testing-library/dom-testing-library/releases/tag/v7.0.0 for more details
+import MutationObserver from '@sheerun/mutationobserver-shim';
+window.MutationObserver = MutationObserver;
---
+++ src/types/thirdparties.d.ts
@@ -0,0 +1,4 @@
+// TODO: remove after create-react-app update jest to v26
+// Add this due to dom-testing-library breaking change, see
+// see setupTest.ts for more details
+declare module '@sheerun/mutationobserver-shim';
